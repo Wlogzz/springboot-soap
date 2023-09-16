923c4f492e636a79cb373166f01ddce1a58d99b7
David García <wlogz@hotmail.com>
2023-09-16 18:20:04 -0500

    Conexión exitosa entre SOAP y Spring Boot

--- src/main/java/com/soap/config/SoapConfig.java
+++ src/main/java/com/soap/config/SoapConfig.java
@@ -1,5 +1,6 @@
 package com.soap.config;
 
+import com.soap.client.SoapClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
@@ -12,5 +13,14 @@ public class SoapConfig {
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
         marshaller.setContextPath("com.soap.wsdl");
         return marshaller;
+    }
+
+    @Bean
+    public SoapClient getSoapClient(Jaxb2Marshaller marshaller){
+        SoapClient soapClient = new SoapClient();
+        soapClient.setDefaultUri("http://www.dneonline.com/calculator.asmx");
+        soapClient.setMarshaller(marshaller); //Serialización
+        soapClient.setUnmarshaller(marshaller); //Deserialización
+        return soapClient;
     }
 }
